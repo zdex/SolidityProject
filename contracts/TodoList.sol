0af312e6b16ac9f76f1ca3a6a1d5733b54bb5bc1
gmaheshwari2006 <gmaheshwari2006@gmail.com>
2020-10-08 20:39:41 -0400

    changes

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.0; //will not work with any version lower than 0.5.0 and because of "^" any version equal to or greateer than 0.6.0
 
 
 contract TodoList {
@@ -11,13 +11,36 @@ contract TodoList {
 	}
 
 	mapping(uint => Task) public tasks;
+
+	event TaskCreated (
+		uint id,
+		string content,
+		bool completed
+	);
+
+	event TaskCompleted(
+		uint id,
+		bool completed
+	);
+
+	struct Addresses {
+      address[]  _items;
+    }
+	 
 	constructor() public {
 		createTask("this is first task");
 	}
 	function createTask(string memory _content) public {
 		taskCount ++;
 		tasks[taskCount] = Task(taskCount, _content, false);
+		emit TaskCreated(taskCount, _content, false);
 	}
 
+ 	function toggleCompleted(uint _id) public {
+    	Task memory _task = tasks[_id];
+    	_task.completed = !_task.completed;
+    	tasks[_id] = _task;
+    	emit TaskCompleted(_id, _task.completed);
+  }
 	 
 }
